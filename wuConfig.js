bf2d6b92053b15c2f9fb43b61707d5b7c29c0401
key762 <95260477+key762@users.noreply.github.com>
2024-12-23 17:20:54 +0800

    Update wuConfig.js

--- wuConfig.js
+++ wuConfig.js
@@ -46,13 +46,15 @@ function doConfig(configFile, cb) {
                     root = root.substring(1);
                 }
                 let newPages = [];
-                for (let page of subPackage.pages) {
-                    let items = page.replace(root, '');
-                    newPages.push(items);
-                    let subIndex = pages.indexOf(root + items);
-                    console.log(root + items, subIndex);
-                    if (subIndex!==-1) {
-                        pages.splice(subIndex, 1);
+                if (subPackage.pages) {
+                    for (let page of subPackage.pages) {
+                        let items = page.replace(root, '');
+                        newPages.push(items);
+                        let subIndex = pages.indexOf(root + items);
+                        console.log(root + items, subIndex);
+                        if (subIndex!==-1) {
+                            pages.splice(subIndex, 1);
+                        }
                     }
                 }
                 subPackage.root = root;
